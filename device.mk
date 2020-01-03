f8dd522c42d3abeb99bf74f3a776c2df32cc3f15
kunal <kunalshah912@gmail.com>
2020-01-03 19:51:45 +0530

    Raphael: Enable in-screen fingerprint HAL
    Signed-off-by: kunal <kunalshah912@gmail.com>

--- device.mk
+++ device.mk
@@ -68,9 +68,6 @@ PRODUCT_PACKAGES += \
     qcom.fmradio
 
 # Fingerprint
-PRODUCT_COPY_FILES += \
-    vendor/arrow/config/permissions/vendor.lineage.biometrics.fingerprint.inscreen.xml:system/etc/permissions/vendor.lineage.biometrics.fingerprint.inscreen.xml
-
 PRODUCT_PACKAGES += \
     lineage.biometrics.fingerprint.inscreen@1.0-service.raphael
 
