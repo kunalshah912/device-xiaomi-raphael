94c3a5b21fcc5962f6edee671610c7a79bc868bd
kunal <kunalshah912@gmail.com>
2020-01-05 23:04:16 +0530

    Revert "raphael: include customized version of ANX Camera"
    This reverts commit 8ab9876822480f6b3baaebdc1628f16a05624397.

--- arrow_raphael.mk
+++ arrow_raphael.mk
@@ -25,6 +25,3 @@ TARGET_BOOT_ANIMATION_RES := 1080
 IS_PHONE := true
 TARGET_MINIMAL_APPS := false
 TARGET_INCLUDE_STOCK_ARCORE := true
-
-# Inherit from custom vendor
-$(call inherit-product, vendor/MiuiCamera/config.mk)
